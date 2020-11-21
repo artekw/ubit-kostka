e795b5f94f3d1589fe6dda44b3e7a3230041131f
Artur Wronowski <arteqw@gmail.com>
2020-11-21 20:30:48 +0100

    prezentacja jak w kostece

--- main.ts
+++ main.ts
@@ -4,5 +4,53 @@ input.onGesture(Gesture.Shake, function () {
 let x = 0
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-    basic.showNumber(x)
+    if (x == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (x == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (x == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (x == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (x == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            `)
+    }
 })
